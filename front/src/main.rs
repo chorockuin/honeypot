36f07932ab52f040b4c6c75f7eb5bdcca60e7500
chorockuin <chorockuin@gmail.com>
2022-09-28 00:03:45 +0900

    Update front to connect backend
    -------------------------------------------------|
    * Subject Rule
      - English only
      - Capitalize first character
      - 50 characters or less
      - Don't use '.'
      - Begin with below word only
        - Add : new features
        - Update : feature
        - Remove : feature
        - Refactor : feature's structure not testcase
        - Fix : bug
        - Verify : feature with testcase
    ------------------------------------------------------------------------------|
    * Body Rule
      - 79 characters or less per line
      - Describe 'what' and 'why' not 'how'

--- front/src/main.rs
+++ front/src/main.rs
@@ -1,125 +1,80 @@
-use yew::{prelude::*, props};
-use serde_json::Value;
-use web_sys::{HtmlInputElement, Text};
+use yew::prelude::*;
+use reqwasm::http::Request;
+use web_sys::HtmlInputElement;
+use js_sys::JsString;
 
 fn main() {
     yew::start_app::<Login>();
 }
 
-#[derive(PartialEq, Debug, Clone)]
-struct Account{
-    id : String,
-    pw : String,
-}
-
 #[function_component(Login)]
 fn login() -> Html{
-    let id = "ID";
-    let pw = "PASSWORD";
+    let input_id = NodeRef::default();
+    let input_id_clone = input_id.clone();
 
-    let id_input_ref = NodeRef::default();
-    let id_input_ref_outer = id_input_ref.clone();
-    let pw_input_ref = NodeRef::default();
-    let pw_input_ref_outer = pw_input_ref.clone();
+    let input_pw = NodeRef::default();
+    let input_pw_clone = input_pw.clone();
 
-    let account_state = use_state_eq::<Option<Account>, _>( || None);
-    let account_state_outer = account_state.clone();
-    let login_state = use_state_eq::<Option<Result>, _>( || None);
-    let login_state_outer = login_state.clone();
+    let login_state = use_state(|| None);
+    let login_state_clone = login_state.clone();
 
     let onclick = Callback::from(move |mouse_event:MouseEvent| {
-        web_sys::console::log_1(&mouse_event.into());
+        let id = input_id.cast::<HtmlInputElement>().unwrap().value();
+        let pw = input_pw.cast::<HtmlInputElement>().unwrap().value();
 
-        let id_input = id_input_ref.cast::<HtmlInputElement>().unwrap();
-        let pw_input = pw_input_ref.cast::<HtmlInputElement>().unwrap();
-        
-        let id_input_v = id_input.value();
-        let pw_input_v = pw_input.value();
-        
-        account_state.set(None);
+        // let login_flag = use_state(|| None);
+        // let login_flag_clone = login_flag.clone();
 
-        let account_state = account_state.clone();
+        wasm_bindgen_futures::spawn_local(async move {
+            let backend = format!("http://host.docker.internal:8081/login?id={id}&pw={pw}");
+            let backend_response = Request::get(&backend).send().await.unwrap().text().await.unwrap();
+            web_sys::console::log_1(&JsString::from(backend_response.clone()));
+            if backend_response == String::from("welcome!") {
+                // login_flag.set(Some(1));
+                web_sys::console::log_1(&"login success!!!".into());
+            }
+            else {
+                // login_flag.set(Some(0));
+                web_sys::console::log_1(&"login fail...".into());            
+            }
+        });
 
-        let test = Account {
-            id : id_input_v,
-            pw : pw_input_v,
-        };
-        account_state.set(Some(test));
+        // match *login_flag_clone {
+        //     Some(v) => {
+        //         login_state.set(Some(v));
+                
+        //     }
+        //     None => {
+        //         login_state.set(Some(0));
+        //     }
+        // }
     });
 
     html!{
         <div>
-            <p>{id}</p>
-            <input ref = {id_input_ref_outer.clone()} type="text"/>
-            <p>{pw}</p>
-            <input ref = {pw_input_ref_outer.clone()} type="text"/>
-            <button {onclick}>{"LOGIN"}</button>
-            <ResultComp account = {(*account_state_outer).clone()} login_state={(login_state_outer).clone()} />
+            <p>{"id"}</p>
+            <input ref = {input_id_clone} type="text"/>
+            <p>{"password"}</p>
+            <input ref = {input_pw_clone} type="text"/>
+            <button {onclick}>{"login"}</button>
+            <LoginResult login_state={*login_state_clone} />
         </div>
     }
 }
 
-#[derive(PartialEq, Debug, Clone)]
-enum Result{
-    Correct,
-    Incorrect,
-}
-
 #[derive(Properties, PartialEq)]
-struct ResultPros{
-    account : Option<Account>,
-    login_state : UseStateHandle<Option<Result>>,
+struct LoginResultProps {
+    login_state : Option<i32>
 }
 
-#[function_component(ResultComp)]
-fn resultcomp(props : &ResultPros) -> Html{
-
-    let account = match &props.account{
-        Some(p) => p,
-        None => return html!{},
+#[function_component(LoginResult)]
+fn login_result(props : &LoginResultProps) -> Html{
+    let result = match &props.login_state {
+        Some(v) => v,
+        None => return html!{}
     };
-
-    let login_state_outer = props.login_state.clone();
-    let login_state = props.login_state.clone();
-
-    let id = account.id.clone();
-    let pw = account.pw.clone();
-
-    let remote = Account{
-        id : String::from("HoneyPot"),
-        pw : String::from("1234"),
-    };
-
-    if id == remote.id && pw == remote.pw {
-        login_state.set(Some(Result::Correct));
-        web_sys::console::log_1(&"로그인 성공!!!".into());
-    }else {
-        login_state.set(Some(Result::Incorrect));  
-        web_sys::console::log_1(&"로그인 실패...".into());          
-    }
-
+    
     html!{
-        <div>
-            <Connection result = {(*login_state_outer).clone()} />
-        </div>
-    }
-}
-
-
-#[derive(Properties, PartialEq)]
-struct LoginResult{
-    result : Option<Result>,
-}
-
-#[function_component(Connection)]
-fn connection(props : &LoginResult) -> Html{
-    let text = match &props.result{
-        None => return html!{<div>{"로그인 해주세요."} </div>},
-        Some(Result::Correct) => "로그인 성공!!!",
-        Some(Result::Incorrect) => "로그인 실패...",
-    };
-
-    html!{
-        <div>{text}</div>
+        <div>{result}</div>
     }
 }
